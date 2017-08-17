0b02d61f0edc0e59d33c914736dfcbf3f5293ed9
guozizhuang <1241472349@qq.com>
2017-08-17 09:36:58 +0800

    配置环境设置路由

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+routes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +40,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(3000,function () {
+    console.log('node is ok');
+})
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+/*
+module.exports = function (app) {
+    app.get('/',function (req,res) {
+        res.render('index',{
+          title:'Express'
+        })
+    })
+}*/
+module.exports = function (app) {
+    ///注册页面
+    app.get('/',function (req,res) {
+        res.render('/reg', {
+            title:"首页"
+        })
+    })
+    //注册行为
+    app.post('/reg',function (req,res) {
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+    })
+    //登录页面
+    app.get('/login',function (req,res) {
+        res.render('login',{
+            title:"登录"
+        })
+    })
+    //登录行为
+    app.post('/login',function (req,res) {
 
-module.exports = router;
+    })
+    //发表页面
+    app.get('/post',function (req,res) {
+        res.render('post',{
+            title:"发表文章"
+        })
+    })
+    //发表行为
+    app.get('/logout',function (req,res) {
+
+    })
+    //退出
+    app.get('/logout',function (req,res) {
+
+    })
+}
